6896b7247a0daf70dd5b00c841a2c021c96181f3
ArthurRamos0 <159791804+ArthurRamos0@users.noreply.github.com>
2024-08-04 14:33:28 -0300

    fim da aula001 e inicio da aula002

---
+++ aula001/index.js
@@ -0,0 +1,9 @@
+// Camel case no JavaScript
+
+var nomeDoAluno = "Arthur Ramos"; // String (texto): ultilize sempre aspas
+var idade = 22; // Number
+var altura = 1.77; //Number
+var estudando = true; // Boolean (booleano: Verdadeiro ou falso)
+
+console.log(nomeDoAluno);
+console.log(typeof nomeDoAluno);  
